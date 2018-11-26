9a8132d04007a534cbec92fa795a9c4e3330a78e
Bart852 <38421693+Bart852@users.noreply.github.com>
2018-11-26 12:25:22 +0100

    eerste 2 constraints af

--- sql/constraints.sql
+++ sql/constraints.sql
@@ -1,6 +1,9 @@
 USE [SBBWorkshopOmgeving]
 GO
 
+--==============================================
+-- check if workshop type = INC, IND etc.
+--==============================================
 ALTER TABLE WORKSHOP
 DROP CONSTRAINT IF EXISTS CK_WorkshopTypes
 GO
@@ -8,3 +11,13 @@ GO
 ALTER TABLE WORKSHOP
 ADD CONSTRAINT CK_WorkshopTypes	CHECK(TYPE IN ('INC', 'IND', 'COM', 'ROC', 'LA'))
 
+--==============================================
+-- check if adviseur is not null for INC
+--==============================================
+
+ALTER TABLE WORKSHOP
+DROP CONSTRAINT IF EXISTS CK_WorkshopAdvisor
+GO
+
+ALTER TABLE WORKSHOP
+ADD CONSTRAINT CK_WorkshopAdvisor CHECK(ADVISEUR_ID IS NOT NULL OR TYPE != 'INC')
