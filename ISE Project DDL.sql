72af24a2331b51293478642d4cb36b9d8737660f
Mark van Tienen <markvantienen@hotmail.com>
2018-11-26 10:42:59 +0100

    Create ISE Project DDL.sql

---
+++ ISE Project DDL.sql
@@ -0,0 +1,377 @@
+/*==============================================================*/
+/* DBMS name:      Microsoft SQL Server 2008                    */
+/* Created on:     26-11-2018 10:42:20                          */
+/*==============================================================*/
+
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('ADVISEUR') and o.name = 'FK_ADVISEUR_ADVISEUR__ORGANISA')
+alter table ADVISEUR
+   drop constraint FK_ADVISEUR_ADVISEUR__ORGANISA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('BESCHIKBAARHEID') and o.name = 'FK_BESCHIKB_BESCHIKBB_WORKSHOP')
+alter table BESCHIKBAARHEID
+   drop constraint FK_BESCHIKB_BESCHIKBB_WORKSHOP
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('CONTACTPERSOON') and o.name = 'FK_CONTACTP_CONTACTPE_ORGANISA')
+alter table CONTACTPERSOON
+   drop constraint FK_CONTACTP_CONTACTPE_ORGANISA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('DEELNEMER') and o.name = 'FK_DEELNEME_DEELNEMER_ORGANISA')
+alter table DEELNEMER
+   drop constraint FK_DEELNEME_DEELNEMER_ORGANISA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('DEELNEMER') and o.name = 'FK_DEELNEME_SECTOR_VA_SECTOR')
+alter table DEELNEMER
+   drop constraint FK_DEELNEME_SECTOR_VA_SECTOR
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('DEELNEMER_IN_WORKSHOP') and o.name = 'FK_DEELNEME_DEELNEMER_DEELNEME')
+alter table DEELNEMER_IN_WORKSHOP
+   drop constraint FK_DEELNEME_DEELNEMER_DEELNEME
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('DEELNEMER_IN_WORKSHOP') and o.name = 'FK_DEELNEME_WORKSHOP__WORKSHOP')
+alter table DEELNEMER_IN_WORKSHOP
+   drop constraint FK_DEELNEME_WORKSHOP__WORKSHOP
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('WORKSHOP') and o.name = 'FK_WORKSHOP_ADVISEUR__ADVISEUR')
+alter table WORKSHOP
+   drop constraint FK_WORKSHOP_ADVISEUR__ADVISEUR
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('WORKSHOP') and o.name = 'FK_WORKSHOP_CONTACTPE_CONTACTP')
+alter table WORKSHOP
+   drop constraint FK_WORKSHOP_CONTACTPE_CONTACTP
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('WORKSHOP') and o.name = 'FK_WORKSHOP_MODULE_VA_MODULE')
+alter table WORKSHOP
+   drop constraint FK_WORKSHOP_MODULE_VA_MODULE
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('WORKSHOP') and o.name = 'FK_WORKSHOP_ORGANISAT_ORGANISA')
+alter table WORKSHOP
+   drop constraint FK_WORKSHOP_ORGANISAT_ORGANISA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('WORKSHOP') and o.name = 'FK_WORKSHOP_WORKSHOPL_WORKSHOP')
+alter table WORKSHOP
+   drop constraint FK_WORKSHOP_WORKSHOPL_WORKSHOP
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('WORKSHOP') and o.name = 'FK_WORKSHOP_WORKSHOP__SECTOR')
+alter table WORKSHOP
+   drop constraint FK_WORKSHOP_WORKSHOP__SECTOR
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ADVISEUR')
+            and   type = 'U')
+   drop table ADVISEUR
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('BESCHIKBAARHEID')
+            and   type = 'U')
+   drop table BESCHIKBAARHEID
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('CONTACTPERSOON')
+            and   type = 'U')
+   drop table CONTACTPERSOON
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('DEELNEMER')
+            and   type = 'U')
+   drop table DEELNEMER
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('DEELNEMER_IN_WORKSHOP')
+            and   type = 'U')
+   drop table DEELNEMER_IN_WORKSHOP
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('MODULE')
+            and   type = 'U')
+   drop table MODULE
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ORGANISATIE')
+            and   type = 'U')
+   drop table ORGANISATIE
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('SECTOR')
+            and   type = 'U')
+   drop table SECTOR
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('WORKSHOP')
+            and   type = 'U')
+   drop table WORKSHOP
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('WORKSHOPLEIDER')
+            and   type = 'U')
+   drop table WORKSHOPLEIDER
+go
+
+/*==============================================================*/
+/* Table: ADVISEUR                                              */
+/*==============================================================*/
+create table ADVISEUR (
+   ADVISEUR_ID          int                  not null,
+   ORGANISATIENUMMER    varchar(15)          not null,
+   ATTRIBUTE_13         varchar(256)         not null,
+   ATTRIBUTE_14         varchar(256)         not null,
+   TELEFOONNUMMER       varchar(256)         null,
+   EMAIL                varchar(256)         null,
+   constraint PK_ADVISEUR primary key nonclustered (ADVISEUR_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: BESCHIKBAARHEID                                       */
+/*==============================================================*/
+create table BESCHIKBAARHEID (
+   WORKSHOPLEIDER_ID    int                  not null,
+   KWARTAAL             char(1)              not null,
+   JAAR                 smallint             not null,
+   AANTAL_UUR           smallint             not null,
+   constraint PK_BESCHIKBAARHEID primary key nonclustered (WORKSHOPLEIDER_ID, KWARTAAL, JAAR, AANTAL_UUR)
+)
+go
+
+/*==============================================================*/
+/* Table: CONTACTPERSOON                                        */
+/*==============================================================*/
+create table CONTACTPERSOON (
+   CONTACTPERSOON_ID    int                  not null,
+   ORGANISATIENUMMER    varchar(15)          not null,
+   VOORNAAM             varchar(256)         not null,
+   ACHTERNAAM           varchar(256)         not null,
+   TELEFOONNUMMER       varchar(256)         null,
+   EMAIL                varchar(256)         null,
+   constraint PK_CONTACTPERSOON primary key nonclustered (CONTACTPERSOON_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: DEELNEMER                                             */
+/*==============================================================*/
+create table DEELNEMER (
+   DEELNEMER_ID         int                  not null,
+   SECTOR_NAAM          varchar(256)         not null,
+   ORGANISATIENUMMER    varchar(15)          null,
+   AANHEF               varchar(4)           not null,
+   ATTRIBUTE_25         varchar(256)         not null,
+   ATTRIBUTE_26         varchar(256)         not null,
+   GEBOORTEDATUM        datetime             not null,
+   EMAIL                varchar(256)         not null,
+   TELEFOONNUMMER       varchar(256)         not null,
+   OPLEIDINGSNIVEAU     varchar(11)          not null,
+   ORGANISATIE_VESTIGINGSPLAATS varchar(256)         not null,
+   ATTRIBUTE_58         bit                  not null,
+   GEWENST_BEGELEIDINGSNIVEAU varchar(256)         null,
+   FUNCTIENAAM          varchar(256)         null,
+   constraint PK_DEELNEMER primary key nonclustered (DEELNEMER_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: DEELNEMER_IN_WORKSHOP                                 */
+/*==============================================================*/
+create table DEELNEMER_IN_WORKSHOP (
+   WORKSHOP_ID          int                  not null,
+   DEELNEMER_ID         int                  not null,
+   VOLGNUMMER           int                  not null,
+   IS_GOEDGEKEURD       bit                  not null,
+   constraint PK_DEELNEMER_IN_WORKSHOP primary key (WORKSHOP_ID, DEELNEMER_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: MODULE                                                */
+/*==============================================================*/
+create table MODULE (
+   MODULE_NUMMER        int                  not null,
+   ATTRIBUTE_48         varchar(256)         not null,
+   constraint PK_MODULE primary key nonclustered (MODULE_NUMMER)
+)
+go
+
+/*==============================================================*/
+/* Table: ORGANISATIE                                           */
+/*==============================================================*/
+create table ORGANISATIE (
+   ORGANISATIENUMMER    varchar(15)          not null,
+   ORGANIATIENAAM       varchar(256)         null,
+   constraint PK_ORGANISATIE primary key nonclustered (ORGANISATIENUMMER)
+)
+go
+
+/*==============================================================*/
+/* Table: SECTOR                                                */
+/*==============================================================*/
+create table SECTOR (
+   SECTOR_NAAM          varchar(256)         not null,
+   constraint PK_SECTOR primary key nonclustered (SECTOR_NAAM)
+)
+go
+
+/*==============================================================*/
+/* Table: WORKSHOP                                              */
+/*==============================================================*/
+create table WORKSHOP (
+   WORKSHOP_ID          int                  not null,
+   WORKSHOPLEIDER_ID    int                  null,
+   CONTACTPERSOON_ID    int                  null,
+   ORGANISATIENUMMER    varchar(15)          null,
+   MODULE_NUMMER        int                  null,
+   ADVISEUR_ID          int                  null,
+   SECTOR_NAAM          varchar(256)         null,
+   DATUM                datetime             null,
+   STARTTIJD            datetime             null,
+   EINDTIJD             datetime             null,
+   HUISNUMMER           varchar(8)           null,
+   STRAATNAAM           varchar(256)         null,
+   POSTCODE             varchar(12)          null,
+   PLAATSNAAM           varchar(256)         null,
+   STATUS               varchar(256)         null,
+   OPMERKING            varchar(255)         null,
+   TYPE                 varchar(3)           null,
+   VERWERKT_BREIN       datetime             null,
+   DEELNEMER_GEGEGEVENS_ONTVANGEN datetime             null,
+   OVK_BEVESTIGING      datetime             null,
+   PRESENTIELIJST_VERSTUURD datetime             null,
+   PRESENTIELIJST_ONTVANGEN datetime             null,
+   BEWIJS_DEELNAME_MAIL_SBB_WSL datetime             null,
+   constraint PK_WORKSHOP primary key nonclustered (WORKSHOP_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: WORKSHOPLEIDER                                        */
+/*==============================================================*/
+create table WORKSHOPLEIDER (
+   WORKSHOPLEIDER_ID    int                  not null,
+   ATTRIBUTE_8          varchar(256)         not null,
+   AVOORNAAMTTRIBUTE_9  varchar(256)         not null,
+   constraint PK_WORKSHOPLEIDER primary key nonclustered (WORKSHOPLEIDER_ID)
+)
+go
+
+alter table ADVISEUR
+   add constraint FK_ADVISEUR_ADVISEUR__ORGANISA foreign key (ORGANISATIENUMMER)
+      references ORGANISATIE (ORGANISATIENUMMER)
+go
+
+alter table BESCHIKBAARHEID
+   add constraint FK_BESCHIKB_BESCHIKBB_WORKSHOP foreign key (WORKSHOPLEIDER_ID)
+      references WORKSHOPLEIDER (WORKSHOPLEIDER_ID)
+go
+
+alter table CONTACTPERSOON
+   add constraint FK_CONTACTP_CONTACTPE_ORGANISA foreign key (ORGANISATIENUMMER)
+      references ORGANISATIE (ORGANISATIENUMMER)
+go
+
+alter table DEELNEMER
+   add constraint FK_DEELNEME_DEELNEMER_ORGANISA foreign key (ORGANISATIENUMMER)
+      references ORGANISATIE (ORGANISATIENUMMER)
+go
+
+alter table DEELNEMER
+   add constraint FK_DEELNEME_SECTOR_VA_SECTOR foreign key (SECTOR_NAAM)
+      references SECTOR (SECTOR_NAAM)
+go
+
+alter table DEELNEMER_IN_WORKSHOP
+   add constraint FK_DEELNEME_DEELNEMER_DEELNEME foreign key (DEELNEMER_ID)
+      references DEELNEMER (DEELNEMER_ID)
+go
+
+alter table DEELNEMER_IN_WORKSHOP
+   add constraint FK_DEELNEME_WORKSHOP__WORKSHOP foreign key (WORKSHOP_ID)
+      references WORKSHOP (WORKSHOP_ID)
+go
+
+alter table WORKSHOP
+   add constraint FK_WORKSHOP_ADVISEUR__ADVISEUR foreign key (ADVISEUR_ID)
+      references ADVISEUR (ADVISEUR_ID)
+go
+
+alter table WORKSHOP
+   add constraint FK_WORKSHOP_CONTACTPE_CONTACTP foreign key (CONTACTPERSOON_ID)
+      references CONTACTPERSOON (CONTACTPERSOON_ID)
+go
+
+alter table WORKSHOP
+   add constraint FK_WORKSHOP_MODULE_VA_MODULE foreign key (MODULE_NUMMER)
+      references MODULE (MODULE_NUMMER)
+go
+
+alter table WORKSHOP
+   add constraint FK_WORKSHOP_ORGANISAT_ORGANISA foreign key (ORGANISATIENUMMER)
+      references ORGANISATIE (ORGANISATIENUMMER)
+go
+
+alter table WORKSHOP
+   add constraint FK_WORKSHOP_WORKSHOPL_WORKSHOP foreign key (WORKSHOPLEIDER_ID)
+      references WORKSHOPLEIDER (WORKSHOPLEIDER_ID)
+go
+
+alter table WORKSHOP
+   add constraint FK_WORKSHOP_WORKSHOP__SECTOR foreign key (SECTOR_NAAM)
+      references SECTOR (SECTOR_NAAM)
+go
+
