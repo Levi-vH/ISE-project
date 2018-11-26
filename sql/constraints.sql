c553726db7298b71d24902a9d3fd2eb8fe517f8d
Bart852 <38421693+Bart852@users.noreply.github.com>
2018-11-26 11:34:02 +0100

    tsqlt template gemaakt

--- sql/constraints.sql
+++ sql/constraints.sql
@@ -1,37 +1,40 @@
-USE [UnitTesting SBB]
+USE [SBBWorkshopOmgeving]
 GO
 
-SELECT *
-FROM WORKSHOP
-WHERE TYPE = 'INC'
+ALTER TABLE WORKSHOP
+DROP CONSTRAINT IF EXISTS CK_WorkshopTypes
+GO
 
 ALTER TABLE WORKSHOP
 ADD CONSTRAINT CK_WorkshopTypes	CHECK(TYPE IN ('INC', 'IND', 'COM', 'ROC', 'LA'))
 
 
 
-USE [UnitTesting COURSE]
+USE [UnitTesting SBB]
 
 EXEC tSQLt.NewTestClass 'testWorkshop';
 
 GO
-ALTER PROCEDURE [testEmployeeCheckConstraint].[test op president met zijn salaris]
+ALTER PROCEDURE [testWorkshop].[test op workshop types]
 AS
 BEGIN
 	--voorbereiden
 		--faken van de tabel, strippen van alle constraints, lege tabel
-	EXEC tSQLt.FakeTable @Tablename = 'dbo.emp'; --dbo is het default schema igv create table 
+	EXEC tSQLt.FakeTable @Tablename = 'dbo.WORKSHOP'; --dbo is het default schema igv create table 
 	--nu de te testen constraint er weer opzetten, deze bestaat namelijk al
-	EXEC tSQLt.ApplyConstraint @Tablename = 'dbo.emp', @ConstraintName = 'CK_emp_President_Salary';
+	EXEC tSQLt.ApplyConstraint @Tablename = 'dbo.WORKSHOP', @ConstraintName = 'CK_WorkshopTypes';
 
 	EXEC tSQLt.ExpectNoException
 	--actie
-	insert into emp(empno,ename,job,born,hired,sgrade,msal,username,deptno)
-	values(null,null,'MANAGER',null,null,null,null,null,null); 
+	insert into WORKSHOP([WORKSHOP_ID], [WORKSHOPLEIDER_ID], [CONTACTPERSOON_ID], [ORGANISATIENUMMER],
+	 [MODULE_NUMMER], [ADVISEUR_ID], [SECTOR_NAAM], [DATUM], [STARTTIJD], [EINDTIJD], [HUISNUMMER], [STRAATNAAM],
+	  [POSTCODE], [PLAATSNAAM], [STATUS], [OPMERKING], [TYPE], [VERWERKT_BREIN], [DEELNEMER_GEGEGEVENS_ONTVANGEN],
+	   [OVK_BEVESTIGING], [PRESENTIELIJST_VERSTUURD], [PRESENTIELIJST_ONTVANGEN], [BEWIJS_DEELNAME_MAIL_SBB_WSL])
+	values(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'INC', NULL, NULL, NULL, NULL, NULL, NULL); 
 END
 GO
 
-EXEC [tSQLt].[Run] '[testEmployeeCheckConstraints].[test op president met zijn salaris]'
+EXEC [tSQLt].[Run] '[testWorkshop].[test op workshop types]'
 
 USE [UnitTesting COURSE]
 
