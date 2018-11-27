f6e0a67c59bba14630728dc92f6fc5ca5a298274
Mark van Tienen <markvantienen@hotmail.com>
2018-11-27 11:56:16 +0100

    dikke testdata update nondeju

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -1,3 +1,6 @@
+USE SBBWorkshopOmgeving
+GO
+
 /*==============================================================*/
 /* Table: ORGANISATIE                                           */
 /*==============================================================*/
@@ -14,16 +17,49 @@ ORDER BY ORGANISATIENAAM
 /*==============================================================*/
 /* Table: ADVISEUR                                              */
 /*==============================================================*/
-WITH orgnum AS
+WITH orgnum AS -- organizationnumber/organisatienummer
 (
-SELECT TOP 300 ORGANISATIENUMMER
+SELECT TOP 300 ORGANISATIENUMMER, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
 FROM [SBBWorkshopOmgeving].[dbo].[ORGANISATIE]
-ORDER BY NEWID()
 ),
+fname AS -- firstname/voornaam
 (
-
+SELECT TOP 300 FirstName, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [AdventureWorksDW2014].[dbo].[DimCustomer]
+),
+lname_email AS -- lastname + email/achternaam + email
+(
+SELECT TOP 300 LastName, CAST(RAND(CHECKSUM(NEWID()))*2 AS INT) randomemail, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [AdventureWorksDW2014].[dbo].[DimCustomer]
+),
+phonenum AS -- phonenumber/telefoonnummer
+(
+SELECT 1 AS id, '0' + CAST(CAST(FLOOR((RAND(CHECKSUM(NEWID()))+6)*100000000) AS INT) AS VARCHAR(9)) AS phonenumber
+UNION ALL
+SELECT id + 1, '0' + CAST(CAST(FLOOR((RAND(CHECKSUM(NEWID()))+6)*100000000) AS INT) AS VARCHAR(9)) AS phonenumber
+FROM phonenum
+WHERE id < 300 -- amount of rows/hoeveelheid rijen
 )
+INSERT INTO	[SBBWorkshopOmgeving].[dbo].[ADVISEUR]
+SELECT ORGANISATIENUMMER, FirstName, LastName, phonenumber,
+email =
+CASE
+	when randomemail = 0 then 
+	lower(left(FirstName,1)+[LastName])+'@hotmail.com'
+	else 
+	lower(left(FirstName,1)+[LastName])+'@gmail.com'
+END
+FROM orgnum o, fname f, lname_email le, phonenum p
+WHERE o.id = f.id
+AND o.id = le.id
+AND o.id = p.id
+OPTION(MAXRECURSION 0)
 go
+/*
+SELECT *
+FROM [SBBWorkshopOmgeving].[dbo].[ADVISEUR]
+ORDER BY ADVISEUR_ID
+*/
 
 /*==============================================================*/
 /* Table: CONTACTPERSOON                                        */
