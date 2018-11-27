9050b216a2cd93689aeb1a2bf39f8aedfd66d26e
Levi Hoogstraten <L.vanHoogstraten@student.han.nl>
2018-11-27 13:18:55 +0100

    added database functionality

--- createworkshop.php
+++ createworkshop.php
@@ -18,7 +18,7 @@ $workshopadress = $workshoppostcode = $workshopcity = $workshopleader = $worksho
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $workshoptype = $_POST["workshoptype"];
     $workshopdate = $_POST["workshopdate"];
-    $contactinfo = check_input($_POST["contactinfo"]);
+   //$contactinfo = check_input($_POST["contactinfo"]);
     $workshopmodule = $_POST["workshopmodule"];
     $workshopcompany = $_POST["workshopcompany"];
     $workshopsector = $_POST["workshopsector"];
@@ -31,24 +31,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $workshopnotes = check_input(@$_POST['workshopnotes']);
 
     //Try to make connection
-    //connectToDB();
+    $conn = connectToDB();
 
     //Run the stored procedure
-    $sql = "exec proc_create_workshop(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    $params = array($workshoptype, $workshopdate, $contactinfo, $workshopmodule, $workshopcompany, $workshopsector, $starttime,
-        $endtime, $workshopadress, $workshoppostcode, $workshopcity, $workshopleader, $workshopnotes);
-    $stmt = sqlsrv_query( $conn, $sql, $params);
-    if( $stmt === false ) {
-        die( print_r( sqlsrv_errors(), true));
-    }
+    $sql = "exec proc_create_workshop ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(1, $workshoptype, PDO::PARAM_STR);
+    $stmt->bindParam(2, $workshopdate, PDO::PARAM_STR);
+    //$stmt->bindParam(3, $contactinfo, PDO::PARAM_STR);
+    $stmt->bindParam(3, $workshopmodule, PDO::PARAM_INT);
+    $stmt->bindParam(4, $workshopcompany, PDO::PARAM_STR);
+    $stmt->bindParam(5, $workshopsector, PDO::PARAM_STR);
+    $stmt->bindParam(6, $starttime, PDO::PARAM_STR);
+    $stmt->bindParam(7, $endtime, PDO::PARAM_STR);
+    $stmt->bindParam(8, $workshopadress, PDO::PARAM_STR);
+    $stmt->bindParam(9, $workshoppostcode, PDO::PARAM_STR);
+    $stmt->bindParam(10, $workshopcity, PDO::PARAM_STR);
+    $stmt->bindParam(11, $workshopleader, PDO::PARAM_STR);
+    $stmt->bindParam(12, $workshopnotes, PDO::PARAM_STR);
+    $stmt->execute();
+    print("Fetch all of the remaining rows in the result set:\n");
+    $result = $stmt->fetchAll();
+    print_r($result);
 }
-
-//mssql_free_statement($sql);
 
 ?>
 <div class="container">
     <h2>Maak een nieuwe workshop</h2>
-    <form class="form-horizontal" action="/createworkshop.php">
+    <form class="form-horizontal" action="createworkshop.php" method="post">
         <div class="form-group">
             <label class="control-label col-sm-2" for="workshoptype">Type workshop:</label>
             <div class="col-sm-10">
