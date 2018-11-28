45e64a30fe81868e85f68860dd4825bf6e5b6918
Levi Hoogstraten <L.vanHoogstraten@student.han.nl>
2018-11-28 13:19:46 +0100

    Fixed header

--- allworkshops.php
+++ allworkshops.php
@@ -8,14 +8,16 @@ include 'functions.php';
     <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container">
     <h2 class="text-info text-center" >Overzicht workshops</h2>
     <div>
-        <table class='table table-striped table-dark'>";
+        <table class='table table-striped table-dark'>
             <tr>
                 <th>Workshoptype</th>
                 <th>Datum</th>
@@ -34,9 +36,12 @@ include 'functions.php';
             $stmt = $conn->prepare($sql);
             $stmt->execute();
 
+
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 $html = '';
-                $html .= '<tr>';
+                //$row[\'WORKSHOP_ID\']'
+                $link = $row['WORKSHOP_ID'];
+                $html .= "<tr onclick=\"window.location='workshop.php?id=$link'\">";
                 $html .= '<td>';
                 $html .= $row['TYPE'];
                 $html .= '</td>';
@@ -61,6 +66,12 @@ include 'functions.php';
             }
             ?>
         </table>
+        <p>Icon buttons:</p>
+        <button class="btn"><i class="fa fa-home"></i></button>
+        <button class="btn"><i class="fa fa-bars"></i></button>
+        <button class="btn"><i class="fa fa-trash"></i></button>
+        <button class="btn"><i class="fa fa-close"></i></button>
+        <button class="btn"><i class="fa fa-folder"></i></button>
     </div>
 </div>
 </body>
