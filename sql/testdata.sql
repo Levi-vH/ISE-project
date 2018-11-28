d333548f253e8e0a0976c673bd4f77228784f3c4
Mark van Tienen <markvantienen@hotmail.com>
2018-11-28 13:56:26 +0100

    big change (testdata)

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -274,6 +274,147 @@ FROM [SBBWorkshopOmgeving].[dbo].[MODULE]
 /* Table: WORKSHOP                                              */
 /*==============================================================*/
 DELETE FROM [SBBWorkshopOmgeving].[dbo].[WORKSHOP]
+;WITH wl_id_sname AS -- workshopleader_id + sectorname/workshopleider_id + sectornaam
+(
+SELECT TOP 250 WORKSHOPLEIDER_ID, SECTORNAAM, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [SBBWorkshopOmgeving].[dbo].[WORKSHOPLEIDER], [SBBWorkshopOmgeving].[dbo].[SECTOR]
+),
+cp_id AS -- contactperson_id/contactpersoon_id
+(
+SELECT TOP 250 CONTACTPERSOON_ID, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [SBBWorkshopOmgeving].[dbo].[CONTACTPERSOON]
+),
+orgnum AS -- organizationnumber/organisatienummer
+(
+SELECT TOP 250 ORGANISATIENUMMER, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [SBBWorkshopOmgeving].[dbo].[ORGANISATIE]
+),
+modnum AS -- modulenumber/modulenummer
+(
+SELECT 1 AS id, FLOOR(RAND(CHECKSUM(NEWID()))*(10-8+1)+1) AS modulenumber
+UNION ALL
+SELECT id + 1, FLOOR(RAND(CHECKSUM(NEWID()))*(10-8+1)+1) AS modulenumber
+FROM modnum
+WHERE id < 250 -- amount of rows/hoeveelheid rijen
+),
+ad_id AS -- advisor_id/adviseur_id
+(
+SELECT TOP 250 ADVISEUR_ID, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [SBBWorkshopOmgeving].[dbo].[ADVISEUR]
+),
+wdate AS -- date/datum
+(
+SELECT 1 AS id, CAST(DATEADD(DAY, RAND(CHECKSUM(NEWID())) * DATEDIFF(DAY, '2020-01-01', '2025-01-01'), '2025-01-01') AS DATE) AS workshopdate
+UNION ALL
+SELECT id + 1, CAST(DATEADD(DAY, RAND(CHECKSUM(NEWID())) * DATEDIFF(DAY, '2020-01-01', '2025-01-01'), '2020-01-01') AS DATE) AS workshopdate
+FROM wdate
+WHERE id < 250 -- amount of rows/hoeveelheid rijen
+),
+stime_etime AS -- starttime + endtime/starttijd + eindtijd
+(
+SELECT 1 AS id, CAST(DATEADD(MINUTE, RAND(CHECKSUM(NEWID())) * DATEDIFF(MINUTE, '09:00:00', '13:00:00'), '09:00:00') AS TIME) AS starttime,
+				CAST(DATEADD(MINUTE, RAND(CHECKSUM(NEWID())) * DATEDIFF(MINUTE, '13:00:00', '17:00:00'), '13:00:00') AS TIME) AS endtime
+UNION ALL
+SELECT id + 1, CAST(DATEADD(MINUTE, RAND(CHECKSUM(NEWID())) * DATEDIFF(MINUTE, '09:00:00', '13:00:00'), '09:00:00') AS TIME) AS starttime,
+			   CAST(DATEADD(MINUTE, RAND(CHECKSUM(NEWID())) * DATEDIFF(MINUTE, '13:00:00', '17:00:00'), '13:00:00') AS TIME) AS endtime
+FROM stime_etime
+WHERE id < 250 -- amount of rows/hoeveelheid rijen
+),
+adrs AS -- address/adres
+(
+SELECT TOP 250 STUFF(AddressLine1, 1, PATINDEX('%[^0-9_ -.]%', AddressLine1)-1, '') AS [address], ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [AdventureWorks2014].[Person].[Address]
+WHERE AddressLine1 LIKE '[0-9]%'
+),
+housenum AS -- housenumber/huisnummer
+(
+SELECT 1 AS id, FLOOR(RAND(CHECKSUM(NEWID()))*(10+90)+1) AS housenumber
+UNION ALL
+SELECT id + 1, FLOOR(RAND(CHECKSUM(NEWID()))*(10+90)+1) AS housenumber
+FROM housenum
+WHERE id < 250 -- amount of rows/hoeveelheid rijen
+),
+pcode AS -- postcode
+(
+SELECT 1 AS id, CAST(FLOOR(RAND(CHECKSUM(NEWID()))*(9000)+1000) AS CHAR(4)) AS pcodenumbers,
+				CHAR(CAST((90-65 )*RAND(CHECKSUM(NEWID())) + 65 AS INT)) AS pcodecharacter1,
+				CHAR(CAST((90-65 )*RAND(CHECKSUM(NEWID())) + 65 AS INT)) AS pcodecharacter2
+UNION ALL
+SELECT id + 1, CAST(FLOOR(RAND(CHECKSUM(NEWID()))*(9000)+1000) AS CHAR(4)) AS pcodenumbers,
+			   CHAR(CAST((90-65 )*RAND(CHECKSUM(NEWID())) + 65 AS INT)) AS pcodecharacter1,
+			   CHAR(CAST((90-65 )*RAND(CHECKSUM(NEWID())) + 65 AS INT)) AS pcodecharacter2
+FROM pcode
+WHERE id < 250 -- amount of rows/hoeveelheid rijen
+),
+pname AS -- placename/plaatsnaam
+(
+SELECT TOP 250 City, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [AdventureWorks2014].[Person].[Address]
+),
+wtype AS
+(
+SELECT 1 id, CAST(RAND(CHECKSUM(NEWID()))*4+1 AS INT) randomtype
+UNION ALL
+SELECT id + 1, CAST(RAND(CHECKSUM(NEWID()))*4+1 AS INT) randomtype
+FROM wtype
+WHERE id < 250 -- amount of rows/hoeveelheid rijen
+)
+INSERT INTO [SBBWorkshopOmgeving].[dbo].[WORKSHOP]
+SELECT WORKSHOPLEIDER_ID,
+CONTACTPERSOON_ID,
+ORGANISATIENUMMER,
+modulenumber,
+ADVISEUR_ID,
+SECTORNAAM,
+workshopdate,
+starttime,
+endtime,
+[address],
+(pcodenumbers + pcodecharacter1 + pcodecharacter2) AS postcode,
+City,
+'bevestigd' AS [status],
+NULL AS OPMERKING,
+[type] =
+CASE
+	WHEN randomtype = 1 THEN 'IND'
+	WHEN randomtype = 2 THEN 'INC'
+	WHEN randomtype = 3 THEN 'COM'
+	ELSE 'LA'
+END,
+NULL AS VERWERKT_BREIN,
+NULL AS DEELNEMER_GEGEGEVENS_ONTVANGEN,
+NULL AS OVK_BEVESTIGING,
+NULL AS PRESENTIELIJST_VERSTUURD,
+NULL AS PRESENTIELIJST_ONTVANGEN,
+NULL AS BEWIJS_DEELNAME_MAIL_SBB_WSL
+FROM wl_id_sname ws, cp_id cid, orgnum o, modnum m, ad_id aid, wdate w, stime_etime se, adrs a, housenum h, pcode p, pname pl, wtype wo
+WHERE ws.id = cid.id
+AND ws.id = o.id
+AND ws.id = m.id
+AND ws.id = aid.id
+AND ws.id = w.id
+AND ws.id = se.id
+AND ws.id = a.id
+AND ws.id = h.id
+AND ws.id = p.id
+AND ws.id = pl.id
+AND ws.id = wo.id
+OPTION (MAXRECURSION 0)
+go
+
+;WITH randomworkshops AS
+(
+SELECT TOP 75 WORKSHOP_ID, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
+FROM [SBBWorkshopOmgeving].[dbo].[WORKSHOP]
+)
+UPDATE [SBBWorkshopOmgeving].[dbo].[WORKSHOP]
+SET WORKSHOPLEIDER_ID = NULL
+FROM [SBBWorkshopOmgeving].[dbo].[WORKSHOP] w INNER JOIN randomworkshops r ON w.WORKSHOP_ID = r.WORKSHOP_ID
+go
+
+UPDATE [SBBWorkshopOmgeving].[dbo].[WORKSHOP]
+SET [STATUS] = 'uitgezet'
+WHERE WORKSHOPLEIDER_ID IS NULL
 go
 /*
 SELECT *
@@ -284,6 +425,7 @@ FROM [SBBWorkshopOmgeving].[dbo].[WORKSHOP]
 /* Table: DEELNEMER_IN_WORKSHOP                                 */
 /*==============================================================*/
 DELETE FROM [SBBWorkshopOmgeving].[dbo].[DEELNEMER_IN_WORKSHOP]
+--INSERT INTO [SBBWorkshopOmgeving].[dbo].[DEELNEMER_IN_WORKSHOP]
 go
 /*
 SELECT *
