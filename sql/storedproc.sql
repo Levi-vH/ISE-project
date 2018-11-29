d8ff4af27215bb021e9ca30da4a84a835c1fa4c4
Mark van Tienen <markvantienen@hotmail.com>
2018-11-29 12:57:23 +0100

    nieuwe proc voor select op deelnemers bij een workshop

--- sql/storedproc.sql
+++ sql/storedproc.sql
@@ -1,54 +1,77 @@
 USE SBBWorkshopOmgeving
 GO
 
-CREATE OR ALTER PROC proc_create_workshop
-  (
-    @workshoptype VARCHAR(3),
-    @workshopdate varchar(10),
-    @modulenummer INT,
-	@organisatienummer VARCHAR(15),
-	@workshopsector VARCHAR(255),
-    @workshopstarttime varchar(10),
-    @workshopendtime varchar(10),
-    @workshopaddress varchar(10),
-    @workshoppostcode VARCHAR(12),
-    @workshopcity VARCHAR(255),
-    @workshopleader VARCHAR(100),
-    @workshopNote VARCHAR(255)
-  )
+/*==============================================================*/
+/* SP Type: SELECT                                              */
+/*==============================================================*/
+
+CREATE OR ALTER PROC proc_request_workshop_participants
+(
+@workshop_id INT
+)
 AS
-  BEGIN
-  set nocount on
-	  /*
+BEGIN
+	SET NOCOUNT ON
 
-		Create a workshop based on the given parameters
+	SELECT		VOLGNUMMER, VOORNAAM, ACHTERNAAM
+	FROM		DEELNEMER_IN_WORKSHOP DW INNER JOIN DEELNEMER D
+	ON			DW.DEELNEMER_ID = D.DEELNEMER_ID
+	WHERE		WORKSHOP_ID = 5--@workshop_id
+	ORDER BY	VOLGNUMMER
+END
+GO
 
-      */
+/*==============================================================*/
+/* SP Type: INSERT                                              */
+/*==============================================================*/
 
-    DECLARE @status VARCHAR(40)
-    SET @status = 'uitgezet'
+CREATE OR ALTER PROC proc_create_workshop
+(
+@workshoptype		VARCHAR(3),
+@workshopdate		varchar(10),
+@modulenummer		INT,
+@organisatienummer	VARCHAR(15),
+@workshopsector		VARCHAR(255),
+@workshopstarttime	varchar(10),
+@workshopendtime	varchar(10),
+@workshopaddress	varchar(10),
+@workshoppostcode	VARCHAR(12),
+@workshopcity		VARCHAR(255),
+@workshopleader		VARCHAR(100),
+@workshopNote		VARCHAR(255)
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	/*
 
-    IF(@workshopleader IS NOT NULL)
-      BEGIN
-        SET @status = 'bevestigd'
-      END
+	Create a workshop based on the given parameters
 
-    -- If a workshop is NOT from IND, the workshopsector MUST be filled in
+	*/
 
-    IF( @workshoptype != 'IND' AND @workshopsector IS NULL )
-      BEGIN
-        RAISERROR('Een workshop met type IND MOET een workshopsector hebben',16,1)
-      END
+	DECLARE @status VARCHAR(40)
+	SET @status = 'uitgezet'
 
-    INSERT INTO WORKSHOP
-    VALUES(@workshopleader,
-      (SELECT CONTACTPERSOON_ID FROM CONTACTPERSOON WHERE ORGANISATIENUMMER = @organisatienummer)
-      ,@organisatienummer,@modulenummer
-      ,(SELECT ADVISEUR_ID FROM ADVISEUR WHERE ORGANISATIENUMMER = @organisatienummer),
-      @workshopsector, @workshopdate,@workshopstarttime,
-      @workshopendtime, @workshopaddress,@workshoppostcode,
-      @workshopcity,@status,
-      @workshopNote, @workshoptype ,null,null,null,null,null,null)
+	IF(@workshopleader IS NOT NULL)
+		BEGIN
+			SET @status = 'bevestigd'
+		END
 
-  END
+	-- If a workshop is NOT from IND, the workshopsector MUST be filled in
+
+	IF( @workshoptype != 'IND' AND @workshopsector IS NULL )
+		BEGIN
+			RAISERROR('Een workshop met type IND MOET een workshopsector hebben',16,1)
+		END
+
+	INSERT INTO WORKSHOP
+	VALUES	(@workshopleader,
+			(SELECT CONTACTPERSOON_ID FROM CONTACTPERSOON WHERE ORGANISATIENUMMER = @organisatienummer)
+			,@organisatienummer,@modulenummer
+			,(SELECT ADVISEUR_ID FROM ADVISEUR WHERE ORGANISATIENUMMER = @organisatienummer),
+			@workshopsector, @workshopdate,@workshopstarttime,
+			@workshopendtime, @workshopaddress,@workshoppostcode,
+			@workshopcity,@status,
+			@workshopNote, @workshoptype ,null,null,null,null,null,null)
+END
 GO
