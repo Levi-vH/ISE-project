a493e5a5daf981f2ae7bb67365a58ed05fcc0587
Mark van Tienen <markvantienen@hotmail.com>
2018-11-30 13:31:22 +0100

    Update testdata.sql

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -638,10 +638,10 @@ SET IS_GOEDGEKEURD = 1
 FROM [SBBWorkshopOmgeving].[dbo].[DEELNEMER_IN_WORKSHOP] dw INNER JOIN appr a ON dw.VOLGNUMMER = a.VOLGNUMMER
 go
 
-/*
 /*==============================================================*/
 /* Table: AANVRAAG				                                */
 /*==============================================================*/
+/*
 ;WITH ad_id_cp_id AS -- advisor_id + contactperson_id/adviseur_id + contactpersoon_id
 (
 SELECT TOP 20 ADVISEUR_ID AS advisor_id, CONTACTPERSOON_ID AS contactperson_id, ROW_NUMBER() OVER (ORDER BY NEWID()) AS id
