6cca357937315996bad8a5808c1b00d2e08ef2ea
Mark van Tienen <markvantienen@hotmail.com>
2018-12-17 11:06:16 +0100

    Create indexen.sql

---
+++ sql/indexen.sql
@@ -0,0 +1,9 @@
+/* =================================================================
+   Author: Mark
+   Create date: 17-12-2018
+   Description: this script is used to create indexes for the
+   'SBBWorkshopOmgeving' database
+   --------------------------------
+   Modified by:
+   Modifications made by :
+   ================================================================ */
