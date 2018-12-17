096f691fda600e367e398a4eca80ccc5450a8a23
Levi Hoogstraten <L.vanHoogstraten@student.han.nl>
2018-12-17 11:05:20 +0100

    test mail function

--- testmail.php
+++ testmail.php
@@ -1,31 +1,11 @@
 <?php
 
-require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+include 'functions.php';
 
-require 'vendor/autoload.php';
+//Test
 
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "testISEB2@gmail.com";
-$mail->Password = "ise!b2MAILTEST";
-$mail->SetFrom("testISEB2@gmail.com");
-$mail->Subject = "Test";
-$mail->Body = "hello";
-$mail->AddAddress("levicheater@hotmail.com");
-if(!$mail->Send()) {
-    echo 'Message was not sent!.';
-    echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-    echo  //Fill in the document.location thing
-    '<script type="text/javascript">
-                        if(confirm("Your mail has been sent"))
-                        document.location = "/";
-        </script>';
-}
+//Use 10 minute mail for testing purposes
+//https://10minutemail.com/10MinuteMail/index.html
+
+sendMail("o4120731@nwytg.net", "Testingss", "Test <br> Deze mail kan HTML bevatten <br>");
 ?>
