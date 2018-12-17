ad3883753afe35c42a279343007103457f54a0ee
Levi Hoogstraten <L.vanHoogstraten@student.han.nl>
2018-12-17 15:05:44 +0100

    fixed stored proc

--- reports.php
+++ reports.php
@@ -4,54 +4,56 @@ include 'functions.php';
 generate_header('Workshop overzicht voor sector');
 ?>
 
-    <div class="container">
-        <h2 class="text-info text-center">Rapportages</h2>
-        <div>
-            <form method="POST">
-                <label>Type uw sector in</label>
-                <input type="text" name="sector"/>
-                <button type="submit">Submit</button>
-            </form>
-            <form method="POST">
-                <label>Druk op de knop voor geannuleerde workshops</label>
-                <button type="submit" name="cancelled">Submit</button>
-            </form>
-            <form method="POST">
-                <label>Type een workshopleider in</label>
-                <input type="text" name="workshopleader"/>
-                <button type="submit">Submit</button>
-            </form>
-            <?php
-            if (isset($_POST['sector'])) {
-                $sectorname = $_POST["sector"];
-                echo '<p>Aantal workshops voor sector ' . strtoupper($sectorname) . ': ' . getCountOfWorkshopsForSector($sectorname) . '</p>';
-            } elseif (isset($_POST['cancelled'])) {
-                $cancelled = getCountOfCancelledWorkshops();
-                echo '<p>Aantal geannuleerde workshops: ' . $cancelled . '</p>';
-            } elseif (isset($_POST['workshopleader'])) {
-                $workshopleader = $_POST['workshopleader'];
-                echo '<p>Aantal workshops van workshopleider ' . $workshopleader . ': ' . getCountOfWorkshopsForWorkshopLeader($workshopleader) . '</p>';
+<div class="container">
+    <h2 class="text-info text-center">Rapportages</h2>
+    <div>
+        <form method="POST">
+            <label>Type uw sector in</label>
+            <input type="text" name="sector"/>
+            <button type="submit">Submit</button>
+        </form>
+        <form method="POST">
+            <label>Druk op de knop voor geannuleerde workshops</label>
+            <button type="submit" name="cancelled">Submit</button>
+        </form>
+        <form method="POST">
+            <label>Type een workshopleider in</label>
+            <input type="text" name="workshopleader"/>
+            <button type="submit">Submit</button>
+        </form>
+        <?php
+        if (isset($_POST['sector'])) {
+            $sectorname = $_POST["sector"];
+            echo '<p>Aantal workshops voor sector ' . strtoupper($sectorname) . ': ' . getCountOfWorkshopsForSector($sectorname) . '</p>';
+        } elseif (isset($_POST['cancelled'])) {
+            $cancelled = getCountOfCancelledWorkshops();
+            echo '<p>Aantal geannuleerde workshops: ' . $cancelled . '</p>';
+        } elseif (isset($_POST['workshopleader'])) {
+            $workshopleader = $_POST['workshopleader'];
+            echo '<p>Aantal workshops van workshopleider ' . $workshopleader . ': ' . getCountOfWorkshopsForWorkshopLeader($workshopleader) . '</p>';
 
-            }
+        }
 
-            pre_r(getCountOfTypeWorkshops());
+        pre_r(getCountOfTypeWorkshops());
 
 
-            ?>
-        </div>
+        ?>
     </div>
+</div>
 <div>
     <script>
-        <?php $dataPoints = array(
-                foreach(getCountOfTypeWorkshops() as $row ) {
-                    array("label" => "Chrome", "y" => 64.02),
-            array("label" => "Firefox", "y" => 12.55),
-            array("label" => "IE", "y" => 8.47),
-            array("label" => "Safari", "y" => 6.08),
-            array("label" => "Edge", "y" => 4.29),
-            array("label" => "Others", "y" => 4.59)
-                }
-        )
+        <?php $dataPoints = getCountOfTypeWorkshops();
+        foreach ($dataPoints as $row) {
+            array("label" => $row['TYPE'], "y" => $row['AANTAL']);
+        }
+        
+
+
+        //            array("label" => "Firefox", "y" => 12.55),
+        //            array("label" => "IE", "y" => 8.47),
+        //            array("label" => "Safari", "y" => 6.08),
+        //            array("label" => "Edge", "y" => 4.29),
+        //            array("label" => "Others", "y" => 4.59)
 
         ?>
         window.onload = function () {
