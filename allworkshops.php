f32bab702a97584d3a2757a1679e4284052a0c08
Bart852 <38421693+Bart852@users.noreply.github.com>
2018-12-20 11:47:53 +0100

    Update allworkshops.php

--- allworkshops.php
+++ allworkshops.php
@@ -1,9 +1,21 @@
 <?php
+include 'header.html';
 include 'functions.php';
-
-generate_header('Workshop overzicht');
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>alle workshops</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css"
+</head>
+<body>
 <div class="container">
     <h2 class="text-info text-center" >Overzicht workshops</h2>
     <div>
@@ -12,9 +24,10 @@ generate_header('Workshop overzicht');
                 <th>Workshoptype</th>
                 <th>Module</th>
                 <th>Datum</th>
-                <th>Workshopleider</th>
+                <th>Starttijd</th>
+                <th>Eindtijd</th>
                 <th>Organisatie</th>
-
+                <th>Aantal deelnemers</th>
             </tr>
             <?php
             //Try to make connection
@@ -22,15 +35,15 @@ generate_header('Workshop overzicht');
 
             //Run the stored procedure
            // $sql = "SELECT * FROM VW_WORKSHOPS";
-            $sql = "EXEC SP_get_workshops";
+            $sql = "EXEC proc_getWorkshops";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
 
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
                 $html = '';
+                //$row[\'WORKSHOP_ID\']'
                 $link = $row['WORKSHOP_ID'];
-                $html .= "<tr onclick=\"window.location='workshop.php?workshop_id=$link'\">";
+                $html .= "<tr onclick=\"window.location='workshop.php?id=$link'\">";
                 $html .= '<td class="align-middle">';
                 $html .= $row['TYPE'];
                 $html .= '</td>';
@@ -41,12 +54,17 @@ generate_header('Workshop overzicht');
                 $html .= date('j F Y', strtotime($row['DATUM']));
                 $html .= '</td>';
                 $html .= '<td class="align-middle">';
-                $html .= $row['ADVISEUR_VOORNAAM'] . ' ' . $row['ADVISEUR_ACHTERNAAM'];
+                $html .= substr($row['STARTTIJD'],0,5);
+                $html .= '</td>';
+                $html .= '<td class="align-middle">';
+                $html .= substr($row['EINDTIJD'],0,5);
                 $html .= '</td>';
                 $html .= '<td class="align-middle">';
                 $html .= $row['ORGANISATIENAAM'];
                 $html .= '</td>';
-
+                $html .= '<td class="align-middle">';
+                $html .= $row['AANTAL_DEELNEMER_AANVRAAG'] . '/16';
+                $html .= '</td>';
                 $html .= '</tr>';
 
                 echo $html;
