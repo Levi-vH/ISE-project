04d5dd0abc40c6b2db0e55e3d5dbe4fc7bb4957f
Levi Hoogstraten <L.vanHoogstraten@student.han.nl>
2019-01-22 13:34:35 +0100

    added contactperons

--- createcontactpersons.php
+++ createcontactpersons.php
@@ -33,7 +33,9 @@ if ($_SESSION['username'] == 'beheerder') {
                 <th>Naam</th>
                 <th>Telefoonnummer</th>
                 <th>Email</th>
+                <th>zet op actief/inactief</th>
                 <th>Verwijder contactpersoon</th>
+                <th>Status</th>
                 <p class="alert-danger warning deletewarning">Let op! Het verwijderen van contactpersonen heeft geen
                     gevolgen voor de al geplande/aangevraagde workshops met deze contactpersoon.</p>
             </tr>
@@ -48,9 +50,8 @@ if ($_SESSION['username'] == 'beheerder') {
                 $stmt->execute();
 
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
                     $html = '';
-                    $html .= '<tr>';
+                    $html .= "<tr>";
                     $html .= '<td>';
                     $html .= $row['VOORNAAM'] . ' ' . $row['ACHTERNAAM'];
                     $html .= '</td>';
@@ -60,49 +61,87 @@ if ($_SESSION['username'] == 'beheerder') {
                     $html .= '<td>';
                     $html .= $row['EMAIL'];
                     $html .= '</td>';
+                    if ($row['IS_ACTIEF'] == 1) {
+                        $html .= '<td>';
+                        $html .= '<a class="fas fa-ban" id="denybutton" onclick="return confirm(\'Weet je zeker dat je deze sector op inactief wilt zetten? \')" href="createcontactpersons.php?contactperson=' . $row['CONTACTPERSOON_ID'] . '&inactiveContactperson=true"></a>';
+                        $html .= '</td>';
+                    } elseif ($row['IS_ACTIEF'] == 0) {
+                        $html .= '<td>';
+                        $html .= '<a class="fas fa-check" id="approvebutton" onclick="return confirm(\'Weet je zeker dat je deze sector op actief wilt zetten? \')" href="createcontactpersons.php?contactperson=' . $row['CONTACTPERSOON_ID'] . '&activeContactperson=true"></a>';
+                        $html .= '</td>';
+                    }
                     $html .= '<td>';
-                    $html .= '<a class="fas fa-times" id="denybutton" onclick="return confirm(\'Weet je zeker dat je deze persoon wilt verwijderen? Zijn of haar gegevens worden niet opgeslagen\')" href=""></a>';
+                    $html .= '<a class="fas fa-times" id="denybutton" onclick="return confirm(\'Weet je zeker dat je deze sector wilt verwijderen?\')" href="createcontactpersons.php?contactperson=' . $row['CONTACTPERSOON_ID'] . '&deleteContactperson=true"></a>';
                     $html .= '</td>';
+                    if ($row['IS_ACTIEF'] == 1) {
+                        $html .= '<td>';
+                        $html .= '<p>ACTIEF</p>';
+                        $html .= '</td>';
+                    } elseif ($row['IS_ACTIEF'] == 0) {
+                        $html .= '<td>';
+                        $html .= '<p>INACTIEF</p>';
+                        $html .= '</td>';
+                    }
                     $html .= '</tr>';
 
                     echo $html;
+
                 }
-            }
-            if (isset($organisation_name)) { ?>
-                <form class="form-horizontal" action="createcontactpersons.php?organisation=<?=$organisation_name?>" method="post">
-                    <h2>Maak nieuwe contactpersoon aan</h2>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2 font-weight-bold" for="name">Voornaam:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="name" required>
+
+                if (isset($_GET['deleteContactperson'])) {
+                    deleteContactperson($_GET['contactperson']);
+                    updatePage('createcontactpersons.php?deleteSuccess');
+                } elseif (isset($_GET['inactiveContactperson'])) {
+                    setInactive('CONTACTPERSOON', 'CONTACTPERSOON_ID', $_GET['contactperson']);
+                    updatePage('createcontactpersons.php?inactiveSuccess');
+                } elseif (isset($_GET['activeContactperson'])) {
+                    setActive('CONTACTPERSOON', 'CONTACTPERSOON_ID', $_GET['contactperson']);
+                    updatePage('createcontactpersons.php?inactiveSuccess');
+                }
+
+                if (isset($_GET['deleteSuccess'])) {
+                    echo '<p class="alert-success warning deletewarning">Verwijderen gelukt!</p>';
+                } elseif (isset($_GET['inactiveSuccess'])) {
+                    echo '<p class="alert-success warning deletewarning">Op inactief/actief gezet!</p>';
+                }
+                if (isset($organisation_name)) { ?>
+                    <form class="form-horizontal"
+                          action="createcontactpersons.php?organisation=<?= $organisation_name ?>"
+                          method="post">
+                        <h2>Maak nieuwe contactpersoon aan</h2>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2 font-weight-bold" for="name">Voornaam:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="name" required>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-6 font-weight-bold" for="surname">Achternaam:</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="surname">
+                        <div class="form-group">
+                            <label class="control-label col-sm-6 font-weight-bold" for="surname">Achternaam:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="surname">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2 font-weight-bold"
-                               for="phonenumber">Telefoonnummer:</label>
-                        <div class="col-sm-10">
-                            <input type="number" class="form-control" name="phonenumber">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2 font-weight-bold"
+                                   for="phonenumber">Telefoonnummer:</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" name="phonenumber">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-2 font-weight-bold" for="email">Email:</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" name="email">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2 font-weight-bold" for="email">Email:</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" name="email">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-success btn-lg">Maak aan</button>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-success btn-lg">Maak aan</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
-                <?php
+                    </form>
+                    <?php
+                }
             }
             if (isset($_POST['name'])) {
                 $contactperson_name = $_POST['name'];
@@ -113,11 +152,11 @@ if ($_SESSION['username'] == 'beheerder') {
                 pre_r($_POST);
 
                 createContactpersoon($_GET['organisation'], $contactperson_name, $contactperson_surname,
-                    $contactperson_phonenumber, $contactperson_email );
+                    $contactperson_phonenumber, $contactperson_email);
                 updatePage('createcontactpersons.php?createSuccess');
             }
 
-            if(isset($_GET['createSuccess'])) {
+            if (isset($_GET['createSuccess'])) {
                 echo '<p class="alert-success warning deletewarning">Contactpersoon toegevoegd!</p>';
             }
 
